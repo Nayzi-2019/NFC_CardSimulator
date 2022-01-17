6c4a49da611b13bfddb5b3b61605787a29c0ec5b
ark <nayzi9999@gmail.com>
2022-01-17 16:43:06 +0800

    add main activity

---
+++ app/src/main/java/com/example/test20220117/GalleryPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.test20220117
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GalleryPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gallery_page)
+    }
+}
